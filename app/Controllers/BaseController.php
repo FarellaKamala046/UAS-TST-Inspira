2e8d20037e373460369889f0a9c072bc3f98de09
FarellaKamala046 <18223046@std.stei.itb.ac.id>
2026-01-05 13:12:21 +0700

    Fix: Menambahkan Global CORS di BaseController agar bisa diakses Vercel

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -39,6 +39,10 @@ abstract class BaseController extends Controller
         // Caution: Do not edit this line.
         parent::initController($request, $response, $logger);
 
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding, Authorization");
+
         // Preload any models, libraries, etc, here.
         // $this->session = service('session');
     }
